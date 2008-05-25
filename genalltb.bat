0c11022461d313ed44e80e8ae63cec05033f6d69
Brent Baccala <cosine@freesoft.org>
2008-05-25 21:09:03 +0000

    Initial revision

---
+++ genalltb.bat
@@ -0,0 +1,42 @@
+hoffman -g kk.xml
+
+hoffman -g kqk.xml
+hoffman -g krk.xml
+hoffman -g kbk.xml
+hoffman -g knk.xml
+hoffman -g kpk.xml
+
+hoffman -g kqkq.xml
+hoffman -g kqkr.xml
+hoffman -g kqkb.xml
+hoffman -g kqkn.xml
+hoffman -g krkr.xml
+hoffman -g krkb.xml
+hoffman -g krkn.xml
+hoffman -g kbkb.xml
+hoffman -g kbkn.xml
+hoffman -g knkn.xml
+
+hoffman -g kqkp.xml
+hoffman -g krkp.xml
+hoffman -g kbkp.xml
+hoffman -g knkp.xml
+hoffman -g kpkp.xml
+
+hoffman -g kqqk.xml
+hoffman -g kqrk.xml
+hoffman -g kqbk.xml
+hoffman -g kqnk.xml
+hoffman -g krrk.xml
+hoffman -g krbk.xml
+hoffman -g krnk.xml
+hoffman -g kbbk.xml
+hoffman -g kbnk.xml
+hoffman -g knnk.xml
+
+hoffman -g kqpk.xml
+hoffman -g krpk.xml
+hoffman -g kbpk.xml
+hoffman -g knpk.xml
+
+hoffman -g kppk.xml
