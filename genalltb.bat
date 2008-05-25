9785604693279b589b34e498dd372580fb4c0292
Brent Baccala <cosine@freesoft.org>
2008-05-25 21:13:01 +0000

    only create htb's if they don't already exist

--- genalltb.bat
+++ genalltb.bat
@@ -1,42 +1,42 @@
-hoffman -g kk.xml
+if not exist kk.htb hoffman -g kk.xml
 
-hoffman -g kqk.xml
-hoffman -g krk.xml
-hoffman -g kbk.xml
-hoffman -g knk.xml
-hoffman -g kpk.xml
+if not exist kqk.htb hoffman -g kqk.xml
+if not exist krk.htb hoffman -g krk.xml
+if not exist kbk.htb hoffman -g kbk.xml
+if not exist knk.htb hoffman -g knk.xml
+if not exist kpk.htb hoffman -g kpk.xml
 
-hoffman -g kqkq.xml
-hoffman -g kqkr.xml
-hoffman -g kqkb.xml
-hoffman -g kqkn.xml
-hoffman -g krkr.xml
-hoffman -g krkb.xml
-hoffman -g krkn.xml
-hoffman -g kbkb.xml
-hoffman -g kbkn.xml
-hoffman -g knkn.xml
+if not exist kqkq.htb hoffman -g kqkq.xml
+if not exist kqkr.htb hoffman -g kqkr.xml
+if not exist kqkb.htb hoffman -g kqkb.xml
+if not exist kqkn.htb hoffman -g kqkn.xml
+if not exist krkr.htb hoffman -g krkr.xml
+if not exist krkb.htb hoffman -g krkb.xml
+if not exist krkn.htb hoffman -g krkn.xml
+if not exist kbkb.htb hoffman -g kbkb.xml
+if not exist kbkn.htb hoffman -g kbkn.xml
+if not exist knkn.htb hoffman -g knkn.xml
 
-hoffman -g kqkp.xml
-hoffman -g krkp.xml
-hoffman -g kbkp.xml
-hoffman -g knkp.xml
-hoffman -g kpkp.xml
+if not exist kqkp.htb hoffman -g kqkp.xml
+if not exist krkp.htb hoffman -g krkp.xml
+if not exist kbkp.htb hoffman -g kbkp.xml
+if not exist knkp.htb hoffman -g knkp.xml
+if not exist kpkp.htb hoffman -g kpkp.xml
 
-hoffman -g kqqk.xml
-hoffman -g kqrk.xml
-hoffman -g kqbk.xml
-hoffman -g kqnk.xml
-hoffman -g krrk.xml
-hoffman -g krbk.xml
-hoffman -g krnk.xml
-hoffman -g kbbk.xml
-hoffman -g kbnk.xml
-hoffman -g knnk.xml
+if not exist kqqk.htb hoffman -g kqqk.xml
+if not exist kqrk.htb hoffman -g kqrk.xml
+if not exist kqbk.htb hoffman -g kqbk.xml
+if not exist kqnk.htb hoffman -g kqnk.xml
+if not exist krrk.htb hoffman -g krrk.xml
+if not exist krbk.htb hoffman -g krbk.xml
+if not exist krnk.htb hoffman -g krnk.xml
+if not exist kbbk.htb hoffman -g kbbk.xml
+if not exist kbnk.htb hoffman -g kbnk.xml
+if not exist knnk.htb hoffman -g knnk.xml
 
-hoffman -g kqpk.xml
-hoffman -g krpk.xml
-hoffman -g kbpk.xml
-hoffman -g knpk.xml
+if not exist kqpk.htb hoffman -g kqpk.xml
+if not exist krpk.htb hoffman -g krpk.xml
+if not exist kbpk.htb hoffman -g kbpk.xml
+if not exist knpk.htb hoffman -g knpk.xml
 
-hoffman -g kppk.xml
+if not exist kppk.htb hoffman -g kppk.xml
