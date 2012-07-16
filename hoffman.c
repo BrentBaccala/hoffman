8cccd3813014d220e12d4536e7df282385a28ca0
Brent Baccala <cosine@freesoft.org>
2012-07-16 21:31:43 +0000

    entry_buffer now a void *, too

--- hoffman.c
+++ hoffman.c
@@ -5428,7 +5428,7 @@ tablebase_t * parse_XML_control_file(char *filename)
     he = gethostbyname(hostname);
 
     xmlNodeSetContent(create_GenStats_node("host"), BAD_CAST he->h_name);
-    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.558 $ $Locker: baccala $");
+    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.559 $ $Locker: baccala $");
     xmlNodeSetContent(create_GenStats_node("args"), BAD_CAST options_string);
     strftime(strbuf, sizeof(strbuf), "%c %Z", localtime(&program_start_time.tv_sec));
     if (! do_restart) {
@@ -7049,7 +7049,7 @@ inline entry_t * fetch_entry_pointer(tablebase_t *tb, index_t index)
 int entries_read_fd = -1;
 int entries_write_fd = -1;
 
-entry_t *entry_buffer = NULL;
+void * entry_buffer = NULL;
 index_t entry_buffer_start = 0;
 int entry_buffer_size = 4096;
 
@@ -13709,7 +13709,7 @@ int main(int argc, char *argv[])
 
     /* Print a greating banner with program version number. */
 
-    fprintf(stderr, "Hoffman $Revision: 1.558 $ $Locker: baccala $\n");
+    fprintf(stderr, "Hoffman $Revision: 1.559 $ $Locker: baccala $\n");
 
     /* Figure how we were called.  This is just to record in the XML output for reference purposes. */
 
