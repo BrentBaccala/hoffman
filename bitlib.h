89538cbe23d90a62bcd35f2e985a2b49e599e329
Brent Baccala <cosine@freesoft.org>
2012-07-17 01:43:52 +0000

    Use macro defines (yuck!) to produce bit field operations for various integer types of different sizes

--- bitlib.h
+++ bitlib.h
@@ -33,7 +33,15 @@
 #ifndef _BITLIB_H
 #define _BITLIB_H       1
 
+#include <inttypes.h>		/* C99 integer types */
+
 typedef unsigned int bitoffset;
+
+/* XXX If we don't have __sync_fetch_and_and_8 and
+ * __sync_fetch_and_or_8, we might have problems with 64 bit words.
+ * Should emulate instead, but right now we don't.  We'd have to check
+ * endianness to emulate correctly.
+ */
 
 #ifdef HAVE_INTEL_ATOMIC_OPS
 
@@ -47,83 +55,99 @@ typedef unsigned int bitoffset;
 
 #endif
 
-inline unsigned int get_unsigned_int_field(void * ptr, bitoffset offset, unsigned int mask)
-{
-    unsigned int * iptr;
-    unsigned int val;
-
-    /* It is truly amazing how much of a difference commenting out this next idiot check makes in
-     * gcc 4.0.4's ability to optimize this code, at least on i386.
-     */
-
-    /* if (offset == -1) return 0; */
-
-    iptr = (unsigned int *) ptr;
-    iptr += offset/(8*sizeof(unsigned int));
-    offset %= 8*sizeof(unsigned int);
-
-    /* little endian - bits counted from LSB
-     *
-     *          iptr+1           iptr
-     *    [----------iiiiii][iiiiii--------]
-     *                            | offset |
-     */
-
-    val = (*iptr >> offset);
-    /* 	| (*(iptr+1) << (8*sizeof(unsigned int) - offset)); */
-
-    if (offset != 0) val |= (*(iptr+1) << (8*sizeof(unsigned int) - offset));
-
-#if 0
-    /* XXX maybe this would be faster if we made it unconditional */
-    if ((mask >> (8*sizeof(unsigned int) - offset)) > 0) {
-	iptr ++;
-	val |= *iptr << offset;
-    }
-#endif
-
-    val &= mask;
-
-    return val;
+#define CREATE_UNSIGNED_FIELD_FUNCTIONS(TYPE, TYPE_WITH_UNDERSCORES)					\
+													\
+inline TYPE get_ ## TYPE_WITH_UNDERSCORES ## _field(void * ptr, bitoffset offset, TYPE mask)		\
+{													\
+    TYPE * iptr;											\
+    TYPE val;												\
+													\
+    /* It is truly amazing how much of a difference commenting out this next idiot check makes in	\
+     * gcc 4.0.4's ability to optimize this code, at least on i386.					\
+     */													\
+													\
+    /* if (offset == -1) return 0; */									\
+													\
+    iptr = (TYPE *) ptr;										\
+    iptr += offset/(8*sizeof(TYPE));									\
+    offset %= 8*sizeof(TYPE);										\
+													\
+    /* little endian - bits counted from LSB								\
+     *													\
+     *          iptr+1           iptr									\
+     *    [----------iiiiii][iiiiii--------]								\
+     *                            | offset |								\
+     */													\
+													\
+    val = (*iptr >> offset);										\
+    /* 	| (*(iptr+1) << (8*sizeof(unsigned int) - offset)); */						\
+													\
+    if (offset != 0) val |= (*(iptr+1) << (8*sizeof(TYPE) - offset));					\
+													\
+    /* XXX maybe this would be faster if we made it unconditional */					\
+    /*													\
+    if ((mask >> (8*sizeof(TYPE) - offset)) > 0) {							\
+	iptr ++;											\
+	val |= *iptr << offset;										\
+    }													\
+    */													\
+													\
+    val &= mask;											\
+													\
+    return val;												\
+}													\
+													\
+inline void set_ ## TYPE_WITH_UNDERSCORES ## _field(void *ptr, bitoffset offset, TYPE mask, TYPE val)	\
+{													\
+    TYPE * iptr;											\
+													\
+    iptr = (TYPE *) ptr;										\
+    iptr += offset/(sizeof(TYPE) * 8);									\
+    offset %= sizeof(TYPE) * 8;										\
+													\
+    /* These operations have to be atomic, since the computer uses 32 or 64 bit words, and those	\
+     * words will contain other entries that other threads may be working on, even though this entry	\
+     * will be locked by the current thread.								\
+     */													\
+													\
+    __bitlib_sync_and(iptr, ~ (mask << offset));							\
+    __bitlib_sync_or(iptr, (val & mask) << offset);							\
+													\
+    if ((offset != 0) && (mask >> (8*sizeof(TYPE) - offset)) > 0) {					\
+	iptr ++;											\
+	__bitlib_sync_and(iptr, ~ (mask >> (8*sizeof(TYPE) - offset)));					\
+	__bitlib_sync_or(iptr, (val & mask) >> (8*sizeof(TYPE) - offset));				\
+    }													\
 }
 
-inline void set_unsigned_int_field(void *ptr, bitoffset offset, unsigned int mask, unsigned int val)
-{
-    unsigned int * iptr;
-
-    iptr = (unsigned int *) ptr;
-    iptr += offset/(sizeof(unsigned int) * 8);
-    offset %= sizeof(unsigned int) * 8;
-
-    /* These operations have to be atomic, since the computer uses 32 or 64 bit words, and those
-     * words will contain other entries that other threads may be working on, even though this entry
-     * will be locked by the current thread.
-     */
-
-    __bitlib_sync_and(iptr, ~ (mask << offset));
-    __bitlib_sync_or(iptr, (val & mask) << offset);
-
-    if ((offset != 0) && (mask >> (8*sizeof(int) - offset)) > 0) {
-	iptr ++;
-	__bitlib_sync_and(iptr, ~ (mask >> (8*sizeof(unsigned int) - offset)));
-	__bitlib_sync_or(iptr, (val & mask) >> (8*sizeof(unsigned int) - offset));
-    }
+#define CREATE_SIGNED_FIELD_FUNCTIONS(TYPE, TYPE_WITH_UNDERSCORES)					\
+													\
+inline TYPE get_ ## TYPE ## _field(void * ptr, bitoffset offset, unsigned TYPE mask)			\
+{													\
+    unsigned TYPE val = get_unsigned_ ## TYPE_WITH_UNDERSCORES ## _field(ptr, offset, mask);		\
+													\
+    /* sign extend */											\
+    if (val > (mask >> 1)) val |= (~ (mask >> 1));							\
+													\
+    return (TYPE) val;											\
+}													\
+													\
+inline void set_ ## TYPE_WITH_UNDERSCORES ## _field(void *ptr, bitoffset offset, unsigned TYPE mask, TYPE val)	\
+{													\
+    set_unsigned_ ## TYPE_WITH_UNDERSCORES ## _field(ptr, offset, mask, (unsigned TYPE) val);		\
 }
 
-inline int get_int_field(void * ptr, bitoffset offset, unsigned int mask)
-{
-    unsigned int val = get_unsigned_int_field(ptr, offset, mask);
+#define CREATE_FIELD_FUNCTIONS(TYPE, TYPE_WITH_UNDERSCORES)						\
+    CREATE_UNSIGNED_FIELD_FUNCTIONS(unsigned TYPE, unsigned_ ## TYPE_WITH_UNDERSCORES)			\
+    CREATE_SIGNED_FIELD_FUNCTIONS(TYPE, TYPE_WITH_UNDERSCORES)
 
-    /* sign extend */
-    if (val > (mask >> 1)) val |= (~ (mask >> 1));
+/* Now use the macros above to actually create the inline functions themselves */
 
-    return (int) val;
-}
+CREATE_FIELD_FUNCTIONS(int, int)
+CREATE_UNSIGNED_FIELD_FUNCTIONS(uint32_t, uint32_t)
+CREATE_UNSIGNED_FIELD_FUNCTIONS(uint64_t, uint64_t)
 
-inline void set_int_field(void *ptr, bitoffset offset, unsigned int mask, int val)
-{
-    set_unsigned_int_field(ptr, offset, mask, (unsigned int) val);
-}
+/* Optimized versions for single bit fields */
 
 inline unsigned int get_bit_field(void * ptr, bitoffset offset)
 {
--- bittest.c
+++ bittest.c
@@ -10,8 +10,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <limits.h>
 
 #include "bitlib.h"
+
+#if 0
+
+#define UNSIGNED_TYPE		uint64_t
+#define UNSIGNED_MAX		UINT64_MAX
+#define PRIu			PRIx64
+#define SET_UNSIGNED_FIELD	set_uint64_t_field
+#define GET_UNSIGNED_FIELD	get_uint64_t_field
+
+#else
+
+#define UNSIGNED_TYPE		unsigned int
+#define UNSIGNED_MAX		UINT_MAX
+#define PRIu			"u"
+#define SET_UNSIGNED_FIELD	set_unsigned_int_field
+#define GET_UNSIGNED_FIELD	get_unsigned_int_field
+
+#endif
+
+#if 0
+#define SIGNED_TYPE		int
+#define SET_SIGNED_FIELD	set_int_field
+#define GET_SIGNED_FIELD	get_int_field
+#endif
 
 int main(int argc, char * argv[])
 {
@@ -22,16 +47,16 @@ int main(int argc, char * argv[])
    * retrieve random unsigned data.
    */
 
-  for (j=1; j<=32; j++) {
-    unsigned int mask = j==32 ? 0xffffffff : ((1<<j) - 1);
-    for (i=0; i<256*8; i++) {
-      unsigned int testval = rand() & mask;
-      set_unsigned_int_field(test_area, i, mask, testval);
-      if (get_unsigned_int_field(test_area, i, mask) != testval) {
-	fprintf(stderr, "set_unsigned_field(test_area, %d, %d, %d), returned %d\n",
-		i, mask, testval, get_unsigned_int_field(test_area, i+j, mask));
+  for (j=1; j<=8*sizeof(UNSIGNED_TYPE); j++) {
+      UNSIGNED_TYPE mask = j==8*sizeof(UNSIGNED_TYPE) ? UNSIGNED_MAX : ((1<<j) - 1);
+      for (i=0; i<256*8; i++) {
+	  UNSIGNED_TYPE testval = rand() & mask;
+	  SET_UNSIGNED_FIELD(test_area, i, mask, testval);
+	  if (GET_UNSIGNED_FIELD(test_area, i, mask) != testval) {
+	      fprintf(stderr, "SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), returned %" PRIu "\n",
+		      i, mask, testval, GET_UNSIGNED_FIELD(test_area, i+j, mask));
+	  }
       }
-    }
   }
 
   /* Check that we don't set any extraneous bits. */
@@ -39,26 +64,28 @@ int main(int argc, char * argv[])
   bzero(test_area, 300);
 
   for (j=1; j<=32; j++) {
-    unsigned int mask = j==32 ? 0xffffffff : ((1<<j) - 1);
-    for (i=0; i<256*8; i++) {
-      unsigned int testval = rand() & mask;
-      set_unsigned_int_field(test_area, i, mask, testval);
-      if (get_unsigned_int_field(test_area, i, mask) != testval) {
-	fprintf(stderr, "set_unsigned_field(test_area, %d, %d, %d), returned %d\n",
-		i, mask, testval, get_unsigned_int_field(test_area, i+j, mask));
+      UNSIGNED_TYPE mask = j==8*sizeof(UNSIGNED_TYPE) ? UNSIGNED_MAX : ((1<<j) - 1);
+      for (i=0; i<256*8; i++) {
+	  UNSIGNED_TYPE testval = rand() & mask;
+	  SET_UNSIGNED_FIELD(test_area, i, mask, testval);
+	  if (GET_UNSIGNED_FIELD(test_area, i, mask) != testval) {
+	      fprintf(stderr, "SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), returned %" PRIu "\n",
+		      i, mask, testval, GET_UNSIGNED_FIELD(test_area, i+j, mask));
+	  }
+	  if (GET_UNSIGNED_FIELD(test_area, i+j, mask) != 0) {
+	      fprintf(stderr, "after SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), "
+		      "GET_UNSIGNED_FIELD(test_area, %d, %" PRIu ") returned %" PRIu "\n",
+		      i, mask, testval, i+j, mask, GET_UNSIGNED_FIELD(test_area, i+j, mask));
+	  }
+	  if (i > j) {
+	      if (GET_UNSIGNED_FIELD(test_area, i-j, mask) != 0) {
+		  fprintf(stderr, "after SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), "
+			  "GET_UNSIGNED_FIELD(test_area, %d, %" PRIu ") returned %" PRIu "\n",
+			  i, mask, testval, i-j, mask, GET_UNSIGNED_FIELD(test_area, i-j, mask));
+	      }
+	  }
+	  SET_UNSIGNED_FIELD(test_area, i, mask, 0);
       }
-      if (get_unsigned_int_field(test_area, i+j, mask) != 0) {
-	fprintf(stderr, "after set_unsigned_field(test_area, %d, %d, %d), get_unsigned_int_field(test_area, %d, %d) returned %d\n",
-		i, mask, testval, i+j, mask, get_unsigned_int_field(test_area, i+j, mask));
-      }
-      if (i > j) {
-	if (get_unsigned_int_field(test_area, i-j, mask) != 0) {
-	  fprintf(stderr, "after set_unsigned_field(test_area, %d, %d, %d), get_unsigned_int_field(test_area, %d, %d) returned %d\n",
-		  i, mask, testval, i-j, mask, get_unsigned_int_field(test_area, i-j, mask));
-	}
-      }
-      set_unsigned_int_field(test_area, i, mask, 0);
-    }
   }
 
   /* Check that we don't clear any extraneous bits. */
@@ -66,60 +93,45 @@ int main(int argc, char * argv[])
   memset(test_area, 0xff, 300);
 
   for (j=1; j<=32; j++) {
-    unsigned int mask = j==32 ? 0xffffffff : ((1<<j) - 1);
-    for (i=0; i<256*8; i++) {
-      unsigned int testval = rand() & mask;
-      set_unsigned_int_field(test_area, i, mask, testval);
-      if (get_unsigned_int_field(test_area, i, mask) != testval) {
-	fprintf(stderr, "set_unsigned_field(test_area, %d, %d, %d), returned %d\n",
-		i, mask, testval, get_unsigned_int_field(test_area, i+j, mask));
+      UNSIGNED_TYPE mask = j==8*sizeof(UNSIGNED_TYPE) ? UNSIGNED_MAX : ((1<<j) - 1);
+      for (i=0; i<256*8; i++) {
+	  UNSIGNED_TYPE testval = rand() & mask;
+	  SET_UNSIGNED_FIELD(test_area, i, mask, testval);
+	  if (GET_UNSIGNED_FIELD(test_area, i, mask) != testval) {
+	      fprintf(stderr, "SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), returned %" PRIu "\n",
+		      i, mask, testval, GET_UNSIGNED_FIELD(test_area, i+j, mask));
+	  }
+	  if (GET_UNSIGNED_FIELD(test_area, i+j, mask) != mask) {
+	      fprintf(stderr, "after SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), "
+		      "GET_UNSIGNED_FIELD(test_area, %d, %" PRIu ") returned %" PRIu "\n",
+		      i, mask, testval, i+j, mask, GET_UNSIGNED_FIELD(test_area, i+j, mask));
+	  }
+	  if (i > j) {
+	      if (GET_UNSIGNED_FIELD(test_area, i-j, mask) != mask) {
+		  fprintf(stderr, "after SET_UNSIGNED_FIELD(test_area, %d, %" PRIu ", %" PRIu "), "
+			  "GET_UNSIGNED_FIELD(test_area, %d, %" PRIu ") returned %" PRIu "\n",
+			  i, mask, testval, i-j, mask, GET_UNSIGNED_FIELD(test_area, i-j, mask));
+	      }
+	  }
+	  SET_UNSIGNED_FIELD(test_area, i, mask, mask);
       }
-      if (get_unsigned_int_field(test_area, i+j, mask) != mask) {
-	fprintf(stderr, "after set_unsigned_field(test_area, %d, %d, %d), get_unsigned_int_field(test_area, %d, %d) returned %d\n",
-		i, mask, testval, i+j, mask, get_unsigned_int_field(test_area, i+j, mask));
-      }
-      if (i > j) {
-	if (get_unsigned_int_field(test_area, i-j, mask) != mask) {
-	  fprintf(stderr, "after set_unsigned_field(test_area, %d, %d, %d), get_unsigned_int_field(test_area, %d, %d) returned %d\n",
-		  i, mask, testval, i-j, mask, get_unsigned_int_field(test_area, i-j, mask));
-	}
-      }
-      set_unsigned_int_field(test_area, i, mask, mask);
-    }
   }
 
   /* Check that we can set and retrieve random signed data. */
 
+#ifdef SIGNED_TYPE
   for (j=1; j<=32; j++) {
-    unsigned int mask = j==32 ? 0xffffffff : ((1<<j) - 1);
-    for (i=0; i<256*8; i++) {
-      int testval = (rand() & mask) - (mask >> 1) - 1;
-      set_int_field(test_area, i, mask, testval);
-      if (get_int_field(test_area, i, mask) != testval) {
-	fprintf(stderr, "Error!: set_int_field(test_area, %d, %d, %d) returned %d (%d)\n",
-		i, mask, testval, get_int_field(test_area, i, mask), get_unsigned_int_field(test_area, i, mask));
+      UNSIGNED_TYPE mask = j==8*sizeof(UNSIGNED_TYPE) ? UNSIGNED_MAX : ((1<<j) - 1);
+      for (i=0; i<256*8; i++) {
+	  SIGNED_TYPE testval = (rand() & mask) - (mask >> 1) - 1;
+	  SET_SIGNED_FIELD(test_area, i, mask, testval);
+	  if (GET_SIGNED_FIELD(test_area, i, mask) != testval) {
+	      fprintf(stderr, "Error!: SET_SIGNED_FIELD(test_area, %d, %d, %d) returned %d (%d)\n",
+		      i, mask, testval, GET_SIGNED_FIELD(test_area, i, mask), GET_UNSIGNED_FIELD(test_area, i, mask));
+	  }
       }
-    }
   }
-
-  bzero(test_area, 300);
-
-  for (j=1; j<=32; j++) {
-    unsigned int mask = j==32 ? 0xffffffff : ((1<<j) - 1);
-    for (i=0; i<256*8; i++) {
-      int testval = (rand() & mask) - (mask >> 1) - 1;
-      set_int_field(test_area, i, mask, testval);
-      if (get_int_field(test_area, i, mask) != testval) {
-	fprintf(stderr, "Error!: set_int_field(test_area, %d, %d, %d) returned %d (%d)\n",
-		i, mask, testval, get_int_field(test_area, i, mask), get_unsigned_int_field(test_area, i, mask));
-      }
-      if (get_unsigned_int_field(test_area, i+j, mask) != 0) {
-	fprintf(stderr, "after set_int_field(test_area, %d, %d, %d), get_unsigned_int_field(test_area, %d, %d) returned %d\n",
-		i, mask, testval, i+j, mask, get_unsigned_int_field(test_area, i+j, mask));
-      }
-      set_unsigned_int_field(test_area, i, mask, 0);
-    }
-  }
+#endif
 
   return 0;
 }
