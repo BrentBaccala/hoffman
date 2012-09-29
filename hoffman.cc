f95b68b81e55ce673ecce4eec4f606a032ca2411
Brent Baccala <cosine@freesoft.org>
2012-09-29 18:04:23 +0000

    comments

--- hoffman.cc
+++ hoffman.cc
@@ -3159,8 +3159,6 @@ index_t local_position_to_combinadic3_index(tablebase_t *tb, local_position_t *p
 	 * reducing the encoding value of a pawn, we use the encoding value, with en-passant
 	 * factored in.  A consequence of this is that we never change the value of an en-passant
 	 * encoded pawn.
-	 *
-	 * XXX we move backwards constructing this... is this right?
 	 */
 
 	if ((piece == tb->white_king) || (piece == tb->black_king)) continue;
@@ -5182,7 +5180,7 @@ tablebase_t * parse_XML_into_tablebase(xmlDocPtr doc, bool is_futurebase)
 	     *
 	     * This information is calculated and then ignored for the 'combinadic' index type.
 	     *
-	     * We never assign a "last overlapping piece" to a pawn because of the difficultly in
+	     * We never assign a "last overlapping piece" to a pawn because of the difficulty in
 	     * handling en passant positions, at until I figured out a simple way of handling them
 	     * in 'combinadic3'.
 	     */
@@ -5582,7 +5580,7 @@ tablebase_t * parse_XML_control_file(char *filename)
     he = gethostbyname(hostname);
 
     xmlNodeSetContent(create_GenStats_node("host"), BAD_CAST he->h_name);
-    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.730 $ $Locker: baccala $");
+    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.731 $ $Locker: baccala $");
     xmlNodeSetContent(create_GenStats_node("args"), BAD_CAST options_string);
     strftime(strbuf, sizeof(strbuf), "%c %Z", localtime(&program_start_time.tv_sec));
     if (! do_restart) {
@@ -14386,7 +14384,7 @@ int main(int argc, char *argv[])
 
     /* Print a greating banner with program version number. */
 
-    fprintf(stderr, "Hoffman $Revision: 1.730 $ $Locker: baccala $\n");
+    fprintf(stderr, "Hoffman $Revision: 1.731 $ $Locker: baccala $\n");
 
     /* Figure how we were called.  This is just to record in the XML output for reference purposes. */
 
