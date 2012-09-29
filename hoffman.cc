31a3fbc3a77edf2af89c824373f77ee7af31dde1
Brent Baccala <cosine@freesoft.org>
2012-09-29 18:09:43 +0000

    use delete[] instead of delete where needed

--- hoffman.cc
+++ hoffman.cc
@@ -5580,7 +5580,7 @@ tablebase_t * parse_XML_control_file(char *filename)
     he = gethostbyname(hostname);
 
     xmlNodeSetContent(create_GenStats_node("host"), BAD_CAST he->h_name);
-    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.731 $ $Locker: baccala $");
+    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.732 $ $Locker: baccala $");
     xmlNodeSetContent(create_GenStats_node("args"), BAD_CAST options_string);
     strftime(strbuf, sizeof(strbuf), "%c %Z", localtime(&program_start_time.tv_sec));
     if (! do_restart) {
@@ -8496,8 +8496,8 @@ extern "C++" {
 	{ }
 
 	~sorting_network() {
-	    if (network) delete network;
-	    if (container_num) delete container_num;
+	    if (network) delete[] network;
+	    if (container_num) delete[] container_num;
 	}
 
 	bool empty(void) {
@@ -9037,7 +9037,7 @@ class memory_proptable {
 	}
 
     ~memory_proptable() {
-	delete data;
+	delete[] data;
     }
 
     class proptable_iterator begin() {
@@ -9102,7 +9102,7 @@ extern "C++" {
 		close(fd);
 	    }
 	    unlink(filename);
-	    delete buffer;
+	    delete[] buffer;
 	}
 
 	bool empty(void) {
@@ -14384,7 +14384,7 @@ int main(int argc, char *argv[])
 
     /* Print a greating banner with program version number. */
 
-    fprintf(stderr, "Hoffman $Revision: 1.731 $ $Locker: baccala $\n");
+    fprintf(stderr, "Hoffman $Revision: 1.732 $ $Locker: baccala $\n");
 
     /* Figure how we were called.  This is just to record in the XML output for reference purposes. */
 
