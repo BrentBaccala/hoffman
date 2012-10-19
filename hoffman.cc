e98f5c69830f33a122b330f56c151e5ea0d6ca24
Brent Baccala <cosine@freesoft.org>
2012-10-19 16:54:08 +0000

    fix tablebase_is_color_symmetric to actually return false

--- hoffman.cc
+++ hoffman.cc
@@ -3718,13 +3718,15 @@ bool tablebase_is_color_symmetric(tablebase_t *tb)
 
     for (int piece = 0; piece < tb->num_pieces; piece ++) {
 
+	int piece2;
+
 	if (pieces_used[piece]) continue;
 
 	if ((tb->legal_squares[piece] != ALL_ONES_BITVECTOR)
 	    && (tb->legal_squares[piece] != LEGAL_PAWN_BITVECTOR))
 	    return false;
 
-	for (int piece2 = 0; piece2 < tb->num_pieces; piece2 ++) {
+	for (piece2 = 0; piece2 < tb->num_pieces; piece2 ++) {
 
 	    if (pieces_used[piece2]) continue;
 
@@ -3739,6 +3741,8 @@ bool tablebase_is_color_symmetric(tablebase_t *tb)
 		break;
 	    }
 	}
+
+	if (piece2 == tb->num_pieces) return false;
     }
 
     return true;
@@ -5267,7 +5271,7 @@ tablebase_t * parse_XML_control_file(char *filename)
     he = gethostbyname(hostname);
 
     xmlNodeSetContent(create_GenStats_node("host"), BAD_CAST he->h_name);
-    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.767 $ $Locker: baccala $");
+    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.768 $ $Locker: baccala $");
     xmlNodeSetContent(create_GenStats_node("args"), BAD_CAST options_string);
     strftime(strbuf, sizeof(strbuf), "%c %Z", localtime(&program_start_time.tv_sec));
     if (! do_restart) {
@@ -14166,7 +14170,7 @@ int main(int argc, char *argv[])
 
     /* Print a greating banner with program version number. */
 
-    fprintf(stderr, "Hoffman $Revision: 1.767 $ $Locker: baccala $\n");
+    fprintf(stderr, "Hoffman $Revision: 1.768 $ $Locker: baccala $\n");
 
     /* Figure how we were called.  This is just to record in the XML output for reference purposes. */
 
