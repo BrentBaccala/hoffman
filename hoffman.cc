f4ba25dd3ab230478fe0e5a9b81a9839cae63769
Brent Baccala <cosine@freesoft.org>
2013-03-10 00:47:11 +0000

    comment

--- hoffman.cc
+++ hoffman.cc
@@ -5312,7 +5312,7 @@ tablebase_t * parse_XML_control_file(char *filename)
     he = gethostbyname(hostname);
 
     xmlNodeSetContent(create_GenStats_node("host"), BAD_CAST he->h_name);
-    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.787 $ $Locker: baccala $");
+    xmlNodeSetContent(create_GenStats_node("program"), BAD_CAST "Hoffman $Revision: 1.788 $ $Locker: baccala $");
     xmlNodeSetContent(create_GenStats_node("args"), BAD_CAST options_string);
     strftime(strbuf, sizeof(strbuf), "%c %Z", localtime(&program_start_time.tv_sec));
     if (! do_restart) {
@@ -6069,8 +6069,6 @@ void invert_colors_of_global_position(global_position_t *global)
  *
  * In addition to back-progagation, this function is also used while probing a set of tablebases to
  * see which one of them matches a given position.
- *
- * XXX need to ASSERT that sizeof(..._pieces_processed_bitvector) is at least MAX_PIECES!!
  *
  * XXX the return value assumes that int holds at least 32 bits
  */
@@ -14023,7 +14021,7 @@ int main(int argc, char *argv[])
 
     /* Print a greating banner with program version number. */
 
-    fprintf(stderr, "Hoffman $Revision: 1.787 $ $Locker: baccala $\n");
+    fprintf(stderr, "Hoffman $Revision: 1.788 $ $Locker: baccala $\n");
 
     /* Figure how we were called.  This is just to record in the XML output for reference purposes. */
 
